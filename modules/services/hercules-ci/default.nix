302032f02b177de04bf3ea7aae0d0afe5bbd3751
Daniel Lucas <dan.frank.lucas@gmail.com>
2023-03-23 10:14:52 +0000

    Hercules inport was failing due to requiring sha256

--- modules/services/hercules-ci/default.nix
+++ modules/services/hercules-ci/default.nix
@@ -1,10 +1,4 @@
 {
-  imports = [
-    ( builtins.fetchTarball "https://github.com/hercules-ci/hercules-ci-agent/archive/stable.tar.gz"
-      + "/module.nix"
-    )
-  ];
-
   services.hercules-ci-agent.enable = true;
   services.hercules-ci-agent.settings.concurrentTasks = 12;
   systemd.services.hercules-ci-agent.environment.HAVE_NT_DATA_SCIENCE_CREDENTIALS = "1";
